625775a0403f9bece71446f987be8c1b3aba146b
Gregor Bell <gregorbell@virginmedia.com>
2017-02-28 17:02:33 +0000

    Finished ambient light, diffuse light and specular light tutorials. Worked more on coursework.

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -25,24 +25,29 @@ bool load_content() {
 	// Sphere
 	meshes["sphere"] = mesh(geometry_builder::create_sphere(unsigned int(20), unsigned int(20)));
 
-	// Rock
-	meshes["rock"] = mesh(geometry("models/Rock1.obj"));
+	// Table
+	meshes["table"] = mesh(geometry("models/Table.obj"));
+
+	// Chair
+	meshes["chair"] = mesh(geometry("models/chair.obj"));
 
 	// Set the transforms for your meshes here
 
 	// plane
 	meshes["plane"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
 
-	// rock
-	meshes["rock"].get_transform().scale = vec3(5.0f, 5.0f, 5.0f);
-	meshes["rock"].get_transform().position = vec3(-10.0f, 10.0f, -30.0f);
-
-	// 2.5x scale, move(-25.0f, 10.0f, -25.0f)
+	// Sphere
 	meshes["sphere"].get_transform().scale = vec3(2.50f, 2.50f, 2.50f);
-	meshes["sphere"].get_transform().position = vec3(-25.0f, 10.0f, -25.0f);
+	meshes["sphere"].get_transform().position = vec3(-50.0f, 5.0f, -25.0f);
+
+	// Table
+	//meshes["table"].get_transform().position = vec3(-50.0f, 50.0f, -25.0f);
+	//meshes["table"].get_transform().scale = vec3(1.0f, 1.0f, 1.0f);
+	//meshes["table"].get_transform().rotate(vec3(135.0f, 5.0f, 0.0f));
+
 	// Load texture
-	tex = texture("textures/grass.jpg");
-	tex2 = texture("textures/checker.png");
+	tex = texture("textures/CGRright_202_color.jpg");
+	tex2 = texture("textures/Wooden_Table_1_default.png");
 
 	// Load in shaders
 	eff.add_shader("shaders/coursework.vert", GL_VERTEX_SHADER);
@@ -93,16 +98,16 @@ bool update(float delta_time) {
 	// Use keyboard to move the camera - WASD
 	vec3 translation(0.0f, 0.0f, 0.0f);
 	if (glfwGetKey(renderer::get_window(), 'W')) {
-		translation.z += 5.0f * delta_time;
+		translation.z += 50.0f * delta_time;
 	}
 	if (glfwGetKey(renderer::get_window(), 'S')) {
-		translation.z -= 5.0f * delta_time;
+		translation.z -= 50.0f * delta_time;
 	}
 	if (glfwGetKey(renderer::get_window(), 'A')) {
-		translation.x -= 5.0f * delta_time;
+		translation.x -= 50.0f * delta_time;
 	}
 	if (glfwGetKey(renderer::get_window(), 'D')) {
-		translation.x += 5.0f * delta_time;
+		translation.x += 50.0f * delta_time;
 	}
 	// Move camera
 	cam2.move(translation);
@@ -127,6 +132,10 @@ bool render() {
 		auto MVP = P * V * M;
 		// Set MVP matrix uniform
 		glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+		// Set material colour 
+		glUniform4fv(eff.get_uniform_location("material_colour"), 1, value_ptr(vec4(1.0f, 1.0f, 1.0f, 1.0f)));
+		// Set ambient intensity - (0.3, 0.3, 0.3, 1.0)
+		glUniform4fv(eff.get_uniform_location("ambient_intensity"), 1, value_ptr(vec4(0.1f, 0.1f, 0.1f, 0.1f)));
 		// *********************************
 		// Bind texture to renderer
 		if (e.first == "plane")
@@ -137,6 +146,7 @@ bool render() {
 		{
 			renderer::bind(tex2, 0);
 		}
+		
 		// Set the texture value for the shader here
 		glUniform1i(eff.get_uniform_location("tex"), 0);
 		glUniform1i(eff.get_uniform_location("tex2"), 1 );
